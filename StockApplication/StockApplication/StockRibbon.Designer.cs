dc8a793ed445bd3e3acd4e896b42e5fc58bdd008
chayasuresh <sureshchaya@gmail.com>
2019-05-18 17:24:45 -0500

    Stock Application

---
+++ StockApplication/StockApplication/StockRibbon.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace StockApplication
+{
+    partial class StockRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public StockRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btnAllStocks = this.Factory.CreateRibbonButton();
+            this.btnIndvStocks = this.Factory.CreateRibbonButton();
+            this.btnClear = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "StocksAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btnAllStocks);
+            this.group1.Items.Add(this.btnIndvStocks);
+            this.group1.Items.Add(this.btnClear);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // btnAllStocks
+            // 
+            this.btnAllStocks.Label = "Get All Stock Prices";
+            this.btnAllStocks.Name = "btnAllStocks";
+            this.btnAllStocks.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnAllStocks_Click);
+            // 
+            // btnIndvStocks
+            // 
+            this.btnIndvStocks.Label = "Get Selected Stock Value";
+            this.btnIndvStocks.Name = "btnIndvStocks";
+            this.btnIndvStocks.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnIndvStocks_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Label = "Clear All Stock Prices";
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btnClear_Click);
+            // 
+            // StockRibbon
+            // 
+            this.Name = "StockRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.StockRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnAllStocks;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnIndvStocks;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btnClear;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal StockRibbon StockRibbon
+        {
+            get { return this.GetRibbon<StockRibbon>(); }
+        }
+    }
+}
---
+++ StockApplication/StockApplication/StockRibbon.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Drawing;
+
+namespace StockApplication
+{
+    public partial class StockRibbon
+    {
+        /// <summary>
+        /// Variables
+        /// Ideally All the Stock Info should Load from an API. 
+        /// For the sample Assignment data purpose, added a Collection
+        /// </summary>
+        #region
+        static Dictionary<string, double> StockDictionary = new Dictionary<string, double>()
+        {
+            {"GOOG", 256.85 },
+            {"AAPL", 556.25 },
+            {"GE", 206.83 },
+            {"TSLA", 226.85 }
+        };
+        static Excel.Worksheet CurrentWorkSheet = null;
+        #endregion
+
+        /// <summary>
+        /// Events
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        #region
+        private void StockRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+            //Worksheet CurrenntSheet = Globals.ThisAddIn.GetActiveWorkSheet();
+           
+            //Header Style - Start
+            Excel.Style HeaderStyle = ActiveSheet().Application.ActiveWorkbook.Styles.Add("HdrStyle");
+            HeaderStyle.Font.Name = "Calibri";
+            HeaderStyle.Font.Size = 12;
+            HeaderStyle.Font.Bold = true;
+            HeaderStyle.Font.Color = System.Drawing.ColorTranslator.ToOle(Color.Black);
+            HeaderStyle.Interior.Color = System.Drawing.ColorTranslator.ToOle(Color.Orange);
+
+            Excel.Range HeaderRange = ActiveSheet().Range["A1", "B1"];
+
+            ActiveSheet().Range["A1"].Value = "Stock Ticker";
+            ActiveSheet().Range["B1"].Value = "Current Stock Value";
+            //CurrenntSheet.Columns.AutoFit();
+            HeaderRange.Style = "HdrStyle";
+            //Header Style - End
+
+            //Info Style - Start
+            Excel.Style InfoStyle = ActiveSheet().Application.ActiveWorkbook.Styles.Add("InfoStyle");
+            InfoStyle.Font.Name = "Calibri";
+            InfoStyle.Font.Size = 11;
+            InfoStyle.Font.Bold = true;
+            InfoStyle.Font.Color = System.Drawing.ColorTranslator.ToOle(Color.Black);
+            InfoStyle.Interior.Color = System.Drawing.ColorTranslator.ToOle(Color.Orange);
+
+            Excel.Range InfoRange = ActiveSheet().Range["A2", "B10"];
+            fillStockItems();
+            //CurrenntSheet.Columns.AutoFit();                  
+            HeaderRange.Style = "InfoStyle";
+            // Info Style -  End
+
+            ActiveSheet().Columns.AutoFit();
+
+        }
+        
+        private void btnAllStocks_Click(object sender, RibbonControlEventArgs e)
+        {
+            ClearStockPrices();
+
+            int i = 2;
+            foreach (string item in StockDictionary.Keys)
+            {
+                ActiveSheet().Range["B" + Convert.ToString(i)].Value = StockDictionary[item];
+                i++;
+            }
+
+        }
+
+        private void btnIndvStocks_Click(object sender, RibbonControlEventArgs e)
+        {
+            ActiveSheet().Range["A" + Convert.ToString(StockDictionary.Count + 2)].Value = string.Empty;
+            Excel.Range activeCell = Globals.ThisAddIn.Application.ActiveCell;
+            double stockVal = GetStockPrice(activeCell.Value);
+
+            if (stockVal== 0)
+            {
+                ActiveSheet().Range["A" + Convert.ToString(StockDictionary.Count + 2)].Value = "Please select a Listed Company";
+            }
+            else
+            {
+                ActiveSheet().Range["B" + activeCell.Row.ToString()].Value = stockVal.ToString();
+            }          
+
+        }
+
+        private void btnClear_Click(object sender, RibbonControlEventArgs e)
+        {
+            ClearStockPrices();
+            
+        }
+        #endregion
+
+        /// <summary>
+        /// Private Metods
+        /// </summary>
+        /// <returns></returns>
+        #region
+        private Excel.Worksheet ActiveSheet()
+        {
+            return CurrentWorkSheet == null ? CurrentWorkSheet = Globals.ThisAddIn.GetActiveWorkSheet(): CurrentWorkSheet;
+        }
+
+        private double GetStockPrice(string CompanyName)
+        {
+            if (CompanyName == string.Empty || CompanyName.ToLower() == "stock ticker" || CompanyName.ToLower() == "current stock value")
+                return 0;
+            else
+            {
+                foreach (string item in StockDictionary.Keys)
+                {
+                    if (CompanyName == item)
+                    {
+                        return StockDictionary[item];
+                    }                    
+                }
+                return 0;
+            }
+        }
+
+        private void fillStockItems()
+        {
+            int i = 2;
+            foreach (string item in StockDictionary.Keys)
+            {
+                ActiveSheet().Range["A" + Convert.ToString(i)].Value = item;
+                i++;
+            }
+        }
+
+        private void ClearStockPrices()
+        {
+            int i = 2;
+            foreach (string item in StockDictionary.Keys)
+            {
+                ActiveSheet().Range["B" + Convert.ToString(i)].Value = string.Empty;
+                i++;
+            }
+        }
+        #endregion
+    }
+}
